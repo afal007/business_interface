0af9b10f3e95c71cd4e042aa23a987774c52a26f
Fal Alexander <falalexandr007@gmail.com>
2020-03-20 23:49:35 +0700

    Web isn't working

--- web/src/main/java/ru/afal/bi/web/controller/AccountController.java
+++ web/src/main/java/ru/afal/bi/web/controller/AccountController.java
@@ -1,6 +1,7 @@
 package ru.afal.bi.web.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.afal.bi.business.logic.model.AccountBalance;
 import ru.afal.bi.business.logic.service.AccountBalanceService;
 
+@Profile({"def", "post"})
 @RestController
 @RequestMapping("/account")
 @RequiredArgsConstructor
@@ -15,7 +17,7 @@ public class AccountController {
 
   private final AccountBalanceService accountBalanceService;
 
-  @GetMapping(value = "/{accountNumber}")
+  @GetMapping("/{accountNumber}")
   public AccountBalance getAccountBalance(@PathVariable String accountNumber) {
     return accountBalanceService.getAccountBalance(accountNumber);
   }
---
+++ web/src/main/java/ru/afal/bi/web/controller/TestController.java
@@ -0,0 +1,12 @@
+package ru.afal.bi.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+  @GetMapping("/home")
+  public String index() {
+    return "home";
+  }
+}
