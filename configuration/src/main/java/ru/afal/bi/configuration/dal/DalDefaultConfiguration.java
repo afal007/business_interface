6f75a5aa01f14d32fa8c1daf346d10bec3ee191d
Fal Alexander <falalexandr007@gmail.com>
2020-03-21 01:44:17 +0700

    Fix profiles

--- configuration/src/main/java/ru/afal/bi/configuration/dal/DalDefaultConfiguration.java
+++ configuration/src/main/java/ru/afal/bi/configuration/dal/DalDefaultConfiguration.java
@@ -2,11 +2,13 @@ package ru.afal.bi.configuration.dal;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import ru.afal.bi.business.gateway.repository.AccountRepository;
 import ru.afal.bi.dal.dao.AccountDao;
 import ru.afal.bi.dal.repository.AccountRepositoryDefault;
 
 @Configuration
+@Profile("!post")
 public class DalDefaultConfiguration {
 
   @Bean
